5bb6fa4e705a01c0431533069526c0a488f1f790
LawlietBlack <kenneth.black@live.com>
2016-04-26 11:14:31 -0700

    Half way through Flask REST API

---
+++ Python/flask-rest-api/notes.md
@@ -0,0 +1,13 @@
+## Flask REST API
+
+1. Set up Flask App
+2. Make Resource Classes
+  get, post, put and delete methods
+3. Wire up classes with Blueprint
+  in model file, wire up blueprint, add resources
+  in app file, import models, register blueprints
+4. Set up Post requirements
+  in __init__, set up reqparse, add arguments
+5. Set up data models
+  import marshall
+  
---
+++ Python/flask-rest-api/resources/reviews.py
@@ -0,0 +1,77 @@
+from flask import jsonify, Blueprint, abort
+
+from flask.ext.restful import (Resource, Api, reqparse, inputs, fields,
+                               marshal, marshal_with, url_for)
+
+import models
+
+review_fields = {
+    'id': fields.Integer,
+    'for_course': fields.String,
+    'rating': fields.Integer,
+    'comment': fields.String(default=''),
+    'created_at': fields.DateTime
+}
+
+
+def review_or_404(review_id):
+    try:
+        review = models.Review.get(models.Review.id==review_id)
+    except models.Course.DoesNotExist:
+        abort(404)
+    else:
+        return review
+
+def add_course(review):
+    review.for_course = url_for('resources.courses.course', id=review.course.id)
+    return review
+
+
+class ReviewList(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('course',
+            required=True,
+            help='No course provided',
+            location=['form', 'json'],
+            type=inputs.positive)
+        self.reqparse.add_argument('rating',
+            required=True,
+            help='No course rating provided',
+            location=['form', 'json'],
+            type=inputs.int_range(1, 5))
+        self.reqparse.add_argument('comment',
+            required=False,
+            nullable=True,
+            location=['form', 'json'],
+            default='')
+        super().__init__()
+
+    def get(self):
+        reviews = [marshal(add_course(review), review_fields)
+            for review in models.Review.select()]
+        return {'reviews': reviews}
+
+    @marshal_with(review_fields)
+    def post(self):
+        args = self.reqparse.parse_args()
+        review = models.Review.create(**args)
+        return (add_course(review), 201, {
+            'Location': url_for('resources.reviews.review', id=review.id)})
+
+
+class Review(Resource):
+    @marshal_with(review_fields)
+    def get(self, id):
+        return add_course(review_or_404(id))
+
+    def put(self, id):
+        return jsonify({'course': 1, 'rating': 5})
+
+    def delete(self, id):
+        return jsonify({'course': 1, 'rating': 5})
+
+reviews_api = Blueprint('resources.reviews', __name__)
+api = Api(reviews_api)
+api.add_resource(ReviewList, '/reviews', endpoint='reviews')
+api.add_resource(Review, '/reviews/<int:id>', endpoint='review')
