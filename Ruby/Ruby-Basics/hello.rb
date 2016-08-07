34036a4399bbf58426a52c5dd967b46eb0faff48
LawlietBlack <kenneth.black@live.com>
2016-08-07 03:49:58 -0700

    Complete Ruby Basics and start Rails To Do App and Drupal Basics

---
+++ Ruby/Ruby-Basics/hello.rb
@@ -0,0 +1,8 @@
+place = 'World'
+string = "Hello #{place}!\nworkspaces is...\t meh...."
+puts string
+
+year = 2016
+puts "The year is #{year}"
+future = 5
+puts "In #{future} years, the year will be #{year + future}"
---
+++ Ruby/Ruby-Basics/methods.rb
@@ -0,0 +1,9 @@
+# Print out that we're adding two numbers together, print sum
+def add(a, b)
+  puts "Adding #{a} and #{b}: "
+  return a + b
+end
+
+puts add(2, 3)
+add(4, 5)
+add(5, 6)
